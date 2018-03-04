e60d5327be09127c16dd409af55bc7309d8a7557
Kidorashi <kishimotick@gmail.com>
2018-03-04 13:25:51 +0300

    Update claster.py

--- claster.py
+++ claster.py
@@ -61,7 +61,7 @@ def word_any(word):
 def word_vowel(word):
     word = re.sub(r"(?<=("+'|'.join(vow)+r"))zss", "ȥȥ", word)
     word = re.sub(r"(?<=("+'|'.join(vow)+r"))c(?=(e|i))", "z", word)
-    word = re.sub(r"(?<=("+'|'.join(vow)+r"))c(?!(" + '|'.join(cons) +r"))", "z", word) # РАБОТАЕТ
+    word = re.sub(r"(?<=("+'|'.join(vow)+r"))c(?!(" + '|'.join(cons) +r"))", "z", word)
     word = re.sub(r"(?<=("+'|'.join(vow)+r"))zss^\\b", "ȥȥ", word)
     word = re.sub(r"(?<=("+'|'.join(vow)+r"))zss\\b", "ȥ", word)
     return word
@@ -103,6 +103,5 @@ while n < len(words):
 for word in words:
     w = word
     word = word_work(word)
-    k+=1
     if word != w:
         print (w + ' -> ' + word)
